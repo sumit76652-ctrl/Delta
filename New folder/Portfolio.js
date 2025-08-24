b2a4296f56fce48fd51f0dca1268e62ac50c91e5
sumit <kumarsumit43252@gmail.com>
2025-08-24 14:47:47 +0530

    jytdfg

--- New folder/Portfolio.js
+++ New folder/Portfolio.js
@@ -2,6 +2,8 @@ var typed = new Typed(".text",{
     strings: ["Frontend Developer", "Web Designer",  ],
     typeSpeed: 100,
     backSpeed: 100,
-    backDelay: 1000,
+    backDelay: 1000 ,
     loop: true,
+    y
+
 });
